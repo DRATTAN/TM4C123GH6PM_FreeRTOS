8c52341cab105304e57f8933fa5f2f29ce72d301
DRATTAN <zhanyu981@gmail.com>
2023-08-23 14:44:42 +0800

    update .gitignore

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/Users/RATTAN/Desktop/ccs\ workspace/TM4C123GH6PM_FreeRTOS/driverlib.lib" -llibc.a
-
